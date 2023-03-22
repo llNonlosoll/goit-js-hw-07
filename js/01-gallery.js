008fc8ae7c1787cf5238aebff7335652c2ad38d2
llNonlosoll <wishmasterar@gmail.com>
2023-03-22 21:43:20 +0100

    first done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,70 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+// Change code below this line
+const galleryList = document.querySelector('.gallery');
+
+// Створюємо галарею
+function createGallery(images) {
+  const makeLiItem = images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`,
+    )
+    .join('');
+
+  galleryList.insertAdjacentHTML('afterbegin', makeLiItem);
+}
+
+createGallery(galleryItems);
+
+// Вішаємо слухача подій на клік для відкриття модалки
+galleryList.addEventListener('click', openModalWindow);
+
+// Функція для відображення клікнутого фото в модалці та вішаємо слухач подій на клавіши
+function createModalImg() {
+  const instance = basicLightbox.create(`
+  <img width="1400" height="900" src="${event.target.dataset.source}">
+  `);
+  instance.show();
+
+  document.addEventListener('keydown', closeModalWindow);
+}
+
+// Функція видалення створеного в модалці фото
+function deleteModalImg() {
+  const instance = document.querySelector('.basicLightbox--visible');
+  instance.remove();
+}
+
+// Функція відкриття модалки
+function openModalWindow(event) {
+  event.preventDefault();
+  const clickedImage = event.target.classList.contains('gallery__image');
+
+  if (!clickedImage) {
+    return;
+  }
+
+  if (event.target) {
+    createModalImg();
+  }
+}
+
+// Функція закриття модалки та видалення слухача подій
+function closeModalWindow(event) {
+  const escKey = 'Escape';
+  if (event.code === escKey) {
+    deleteModalImg();
+
+    document.removeEventListener('keydown', closeModalWindow);
+  }
+}
